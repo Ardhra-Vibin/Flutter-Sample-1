ef51a34410a25b462b25145e8808541efa0e904c
Ardhra-Vibin <ardhravibin@gmail.com>
2023-01-27 15:36:43 +0400

    initial commit to branch1

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Sample 2',
+      title: 'BRANCH 11111',
       theme: ThemeData(
         // This is the theme of your application.
         //
